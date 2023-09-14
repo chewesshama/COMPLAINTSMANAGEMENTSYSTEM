a036ba1c3063f34847cca6080b2ac1932cfcdbd9
chewesshama <cochewes99@gmail.com>
2023-09-14 13:56:32 +0300

    adding some custom error templates 404, 403 and 500 errors

--- COMPLAINTSMANAGEMENTSYSTEM/urls.py
+++ COMPLAINTSMANAGEMENTSYSTEM/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, re_path,include
+from django.urls import path,include
 from complaints.views import custom_404_view, custom_403_view, custom_500_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('complaints.urls'), name="complaints"),
     
-
 ]
 
 handler403 = custom_403_view
